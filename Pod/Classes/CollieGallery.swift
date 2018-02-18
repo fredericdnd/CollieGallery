a76db500b54d4890dc0160d62fa91ef1b09099e3
Frédéric Dinand <frederic.dinand.developer@gmail.com>
2018-02-18 18:48:10 +0100

    - Fade caption view

--- Pod/Classes/CollieGallery.swift
+++ Pod/Classes/CollieGallery.swift
@@ -666,3 +666,19 @@ open class CollieGallery: UIViewController, UIScrollViewDelegate, CollieGalleryV
         sourceViewController.present(self, animated: type.animated, completion: nil)
     }
 }
+
+// MARK: EXTENSION
+extension UIView {
+    
+    // MARK: FADE TRANSITION - FUNCTION
+    func fadeTransition(_ duration:CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseInEaseOut)
+        animation.type = kCATransitionFade
+        animation.duration = duration
+        layer.add(animation, forKey: kCATransitionFade)
+    }
+    
+}
+
